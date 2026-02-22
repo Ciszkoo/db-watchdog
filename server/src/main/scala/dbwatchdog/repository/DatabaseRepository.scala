d97338e13f896c8ba5b544d7b707108c1b8cd465
Pawel Olszko <pawel.olszko.p@gmail.com>
2026-02-22 15:56:07 +0100

    feat: table for storing databases information

---
+++ server/src/main/scala/dbwatchdog/repository/DatabaseRepository.scala
@@ -0,0 +1,42 @@
+package dbwatchdog.repository
+
+import java.util.UUID
+
+import doobie.*
+import doobie.implicits.*
+import doobie.postgres.implicits.*
+
+import dbwatchdog.database.TableFragment
+import dbwatchdog.domain.{CreateDatabase, Database}
+
+trait DatabaseRepository extends TableFragment[UUID, Database] {
+
+  val tableName: String = "databases"
+  val columns: List[String] = List(
+    "id",
+    "engine",
+    "host",
+    "port",
+    "password",
+    "\"user\"",
+    "password",
+    "\"schema\"",
+    "created_at",
+    "updated_at"
+  )
+
+  def insert(input: CreateDatabase): ConnectionIO[Database]
+}
+
+object DatabaseRepository {
+
+  def make: DatabaseRepository = new DatabaseRepository {
+
+    def insert(input: CreateDatabase): ConnectionIO[Database] =
+      (fr"INSERT INTO" ++ tableF ++ fr"(engine, host, port, \"user\", password, \"schema\")" ++
+        fr"VALUES (${input.engine}, ${input.host}, ${input.port}, ${input.user}, ${input.password}, ${input.schema})" ++
+        returningF)
+        .query[Database]
+        .unique
+  }
+}
